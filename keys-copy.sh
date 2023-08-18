963d7c2d734d29d8e62e519ebb964300fbdae3a9
PerennaSec <122575063+PerennaSec@users.noreply.github.com>
2023-08-18 13:48:24 -0400

    Add files via upload

---
+++ keys-copy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [ -z "$1" ]
+  then echo -e "Please Supply Path to Destination. \n"
+  exit
+fi
+
+dirpath="$1"
+if [ ! -d "${dirpath}/keystore" ]
+  then echo -e "Keystore Not Found! \n"
+  echo -e "Supply Keystore and Try Again. \n"
+  exit
+else
+  cd ${dirpath}/keystore
+  cp -v ./efikeys/DB.{key,crt} /etc/MY/efikeys
+  cp -v ./signing_key.{pem,x509} /etc/MY/
+  exit
+fi
+
+
+
---
+++ keys-rm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+rm -v /etc/MY/efikeys/DB.{key,crt}
+rm -v /etc/MY/signing_key.{pem,x509}
+
+exit
