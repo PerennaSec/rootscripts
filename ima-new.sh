92fba57837383b85a32449b73cb13ddab5c28363
PerennaSec <122575063+PerennaSec@users.noreply.github.com>
2023-08-18 13:46:00 -0400

    Update ima-new.sh

--- ima-new.sh
+++ ima-new.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#* Sign ALL NEW FILES for use with Integrity Measurement Architecture *#
+
 find / -fstype ext4 -type f -uid 0 -executable -mtime 0 -exec evmctl ima_sign -a sha512 -k /etc/MY/efikeys/DB.key '{}' \; 
 
 exit
